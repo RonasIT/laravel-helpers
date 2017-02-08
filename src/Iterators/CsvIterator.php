5abb45a1fbca92f1f2079e3a329f9fe72265fcfa
Roman Dubrovin <rdubrovin@ronasit.com>
2017-02-08 11:13:32 +0600

    #-: Added iterators

---
+++ src/Iterators/CsvIterator.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: roman
+ * Date: 25.11.16
+ * Time: 10:05
+ */
+
+namespace App\Support\Iterators;
+
+use Iterator;
+
+class CsvIterator implements Iterator {
+    protected $currentCsvLine = [];
+    protected $currentRow = 0;
+    protected $file;
+    protected $filePath;
+
+    public function __construct($filePath) {
+        $this->file = fopen($filePath, 'r');
+        $this->filePath = $filePath;
+    }
+
+    function rewind() {
+        fclose($this->file);
+
+        $this->file = fopen($this->filePath, 'r');
+
+        $this->currentCsvLine = fgetcsv($this->file);
+        $this->currentRow = 0;
+    }
+
+    function current() {
+        return $this->currentCsvLine;
+    }
+
+    function key() {
+        return $this->currentRow;
+    }
+
+    function next() {
+        $this->currentCsvLine = fgetcsv($this->file);
+        $this->currentRow++;
+    }
+
+    function valid() {
+        return $this->currentCsvLine !== false;
+    }
+}
---
+++ src/Iterators/DBIterator.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: roman
+ * Date: 25.11.16
+ * Time: 10:15
+ */
+
+namespace App\Support\Iterators;
+
+use Iterator;
+
+class DBIterator implements Iterator {
+    protected $currentItem = [];
+    protected $sample = [];
+    protected $query;
+    protected $itemsPerPage;
+    protected $position;
+
+    public function __construct($query, $itemsPerPage = 100) {
+        $this->query = $query;
+        $this->itemsPerPage = $itemsPerPage;
+    }
+
+    public function rewind() {
+        $this->loadSample();
+    }
+
+    public function current() {
+        return $this->sample['data'][$this->position];
+    }
+
+    public function key() {
+        return $this->sample['data'][$this->position]['id'];
+    }
+
+    public function next() {
+        $this->position++;
+
+        if ($this->isEndOfSample()) {
+            $this->loadSample($this->sample['current_page'] + 1);
+        }
+    }
+
+    public function valid() {
+        return !($this->isLastSample() && $this->isEndOfSample());
+    }
+
+    public function isEndOfSample() {
+        return $this->position >= count($this->sample['data']);
+    }
+
+    public function isLastSample() {
+        return $this->sample['current_page'] >= $this->sample['last_page'];
+    }
+
+    public function loadSample($page = 1) {
+        $this->sample = $this->query
+            ->paginate($this->itemsPerPage, ['*'], 'page', $page)
+            ->toArray();
+
+        $this->position = 0;
+    }
+}
