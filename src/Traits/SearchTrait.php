379e13cd8a4cb0d5574f1b3c51e993fb33a352be
Roman Dubrovin <rdubrovin@ronasit.com>
2018-07-06 11:26:16 +0600

    #-: fixed bug in filterBy

--- src/Traits/SearchTrait.php
+++ src/Traits/SearchTrait.php
@@ -24,7 +24,7 @@ trait SearchTrait
         }
 
         if (array_has($this->filter, $field)) {
-            $this->query->where($preparedField, $this->filter[$field]);
+            $this->query->where($field, $this->filter[$field]);
         }
 
         return $this;
