a60616e320cee3eef4b43e6015c9895e072fd766
RuslanKasymov <ruslan.kassymov97@mail.ru>
2019-08-26 21:45:23 +0600

    #22: update test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,28 +1,119 @@
 <?php
 
-namespace RonasIT\Support\Tests;
+namespace App\Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTest;
+use Carbon\Carbon;
+use App\Models\JobUser;
+use Tymon\JWTAuth\JWTAuth;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Contracts\Console\Kernel;
 use RonasIT\Support\Traits\FixturesTrait;
+use Illuminate\Contracts\Auth\Authenticatable;
+use RonasIT\Support\AutoDoc\Tests\AutoDocTestCase;
 
-class TestCase extends BaseTest
+abstract class TestCase extends AutoDocTestCase
 {
     use FixturesTrait;
 
-    /**
-     * The base URL to use while testing the application.
-     *
-     * @var string
-     */
-    protected $baseUrl = 'http://localhost';
+    protected $jwt;
+    protected $auth;
 
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->artisan('cache:clear');
+        $this->artisan('migrate');
+
+        $this->loadTestDump(['migrations', 'password_resets', 'interview_types', 'questions', 'answers', 'character_types', 'adaptation_map'],
+            ['migrations', 'password_resets', 'settings', 'questions', 'answers']);
+
+        $this->auth = app(JWTAuth::class);
+
+        Mail::fake();
+    }
+
     public function createApplication()
     {
-        return require __DIR__ . '/../bootstrap/app.php';
+        $app = require __DIR__ . '/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
+    public function actingAs(Authenticatable $user, $driver = null)
+    {
+        $this->jwt = $this->auth->fromUser($user);
+
+        return $this;
+    }
+
+    public function call($method, $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null)
+    {
+        if (!empty($this->jwt)) {
+            $server['HTTP_AUTHORIZATION'] = "Bearer {$this->jwt}";
+        }
+
+        return parent::call($method, $uri, $parameters, $cookies, $files, $server, $content);
+    }
+
+    public function tearDown()
+    {
+        $this->beforeApplicationDestroyed(function () {
+            DB::disconnect();
+        });
+
+        parent::tearDown();
+    }
+
+    /**
+     * Data should looks like following construction:
+     * [
+     *   [
+     *      'emails' => string|array ,
+     *      'fixture' => 'expected_rendered_fixture.html',
+     *      'subject' => string|null
+     *   ]
+     * ]
+     *
+     * @param string $mailableClass
+     * @param array $data
+     */
+    protected function assertMailEquals($mailableClass, $data)
+    {
+        $index = 0;
+
+        Mail::assertSent($mailableClass, function ($mail) use ($data, &$index) {
+            $sentEmails = Arr::pluck($mail->to, 'address');
+            $currentMail = Arr::get($data, $index);
+            $emails = Arr::wrap($currentMail['emails']);
+            $subject = Arr::get($currentMail, 'subject');
+
+            if (!empty($subject)) {
+                $this->assertEquals($currentMail['subject'], $mail->subject);
+            }
+
+            $this->assertEquals(count($mail->to), count($emails));
+
+            $emailList = implode(',', $sentEmails);
+
+            foreach ($emails as $email) {
+                $this->assertContains($email, $sentEmails, "Block \"To\" on {$index} step don't contains {$email}. Contains only {$emailList}.");
+            }
+
+            $this->assertEquals(
+                $this->getFixture($currentMail['fixture']),
+                view($mail->view, $mail->getData())->render(),
+                "Fixture {$currentMail['fixture']} does not equals rendered mail."
+            );
+
+            $index++;
+
+            return true;
+        });
     }
 }
