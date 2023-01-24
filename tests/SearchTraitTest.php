27ccfbbf126bdab382221efac24b258117c40e4b
roman2027@internet.ru <roman2027@internet.ru>
2023-01-24 19:14:04 +0600

    feat: change test Refs: #44

--- tests/SearchTraitTest.php
+++ tests/SearchTraitTest.php
@@ -4,24 +4,10 @@ namespace RonasIT\Support\Tests;
 
 use RonasIT\Support\Repositories\BaseRepository;
 use RonasIT\Support\Tests\Support\Traits\MockTrait;
-use ReflectionProperty;
 
 class SearchTraitTest extends HelpersTestCase
 {
     use MockTrait;
-
-    protected BaseRepository $baseRepositoryClass;
-    protected ReflectionProperty $onlyTrashedProperty;
-
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        $this->baseRepositoryClass = new BaseRepository();
-
-        $this->onlyTrashedProperty = new ReflectionProperty(BaseRepository::class, 'onlyTrashed');
-        $this->onlyTrashedProperty->setAccessible('pubic');
-    }
 
     public function testSearchQueryWithOnlyTrashed()
     {
