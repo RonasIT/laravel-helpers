ae2c97437743c45b26324fa7ecdecebe1ca9c453
roman2027@internet.ru <roman2027@internet.ru>
2023-01-24 18:17:23 +0600

    feat: test onlyTrashed Refs: #44

--- tests/SearchTraitTest.php
+++ tests/SearchTraitTest.php
@@ -10,34 +10,24 @@ class SearchTraitTest extends HelpersTestCase
 {
     use MockTrait;
 
-    protected BaseRepository $baseRepository;
+    protected BaseRepository $baseRepositoryClass;
     protected ReflectionProperty $onlyTrashedProperty;
 
     public function setUp(): void
     {
         parent::setUp();
 
-        $this->baseRepository = new BaseRepository();
+        $this->baseRepositoryClass = new BaseRepository();
 
         $this->onlyTrashedProperty = new ReflectionProperty(BaseRepository::class, 'onlyTrashed');
         $this->onlyTrashedProperty->setAccessible('pubic');
     }
 
-    public function testSearchQueryOnlyTrashed()
-    {
-        $mock = $this->mockClass(BaseRepository::class, ['searchQuery', 'onlyTrashed']);
-
-        $mock->expects($this->once())->method('searchQuery')->with(['only_trashed' => true])->willReturnSelf();
-        $mock->expects($this->once())->method('onlyTrashed')->willReturnSelf();
-        $mock->searchQuery(['only_trashed' => true]);
-        $mock->onlyTrashed();
-    }
-
     public function testOnlyTrashed()
     {
-        $this->baseRepository->onlyTrashed();
+        $this->baseRepositoryClass->onlyTrashed();
 
-        $onlyTrashed = $this->onlyTrashedProperty->getValue($this->baseRepository);
+        $onlyTrashed = $this->onlyTrashedProperty->getValue($this->baseRepositoryClass);
 
         $this->assertEquals(true, $onlyTrashed);
     }
