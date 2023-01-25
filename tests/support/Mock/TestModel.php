fad9a8f2a835380a31f68e5323c92f13957d8a76
roman2027@internet.ru <roman2027@internet.ru>
2023-01-25 15:53:20 +0600

    feat: change test Refs: #44

---
+++ tests/support/Mock/TestModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace RonasIT\Support\Tests\Support\Mock;
+
+use Illuminate\Database\Eloquent\Model;
+use RonasIT\Support\Traits\ModelTrait;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TestModel extends Model
+{
+    use ModelTrait, SoftDeletes;
+}
