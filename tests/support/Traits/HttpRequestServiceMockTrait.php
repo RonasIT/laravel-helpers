e88dd572302109d6d4c2bf5a4a17e8716b8244d1
Vitaly Iskrin <viskrin@ronasit.com>
2023-12-17 14:31:38 -0600

    feat: unify tests

--- tests/support/Traits/HttpRequestServiceMockTrait.php
+++ tests/support/Traits/HttpRequestServiceMockTrait.php
@@ -4,25 +4,22 @@ namespace RonasIT\Support\Tests\Support\Traits;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Response as GuzzleResponse;
+use RonasIT\Support\Traits\MockClassTrait;
 use RonasIT\Support\Traits\WithConsecutiveTrait;
 
 trait HttpRequestServiceMockTrait
 {
-    use MockTrait;
+    use MockClassTrait;
     use WithConsecutiveTrait;
 
     protected function mockGuzzleClient($method, $arguments, $response = null): void
     {
-        $mock = $this->mockClass(Client::class, [$method]);
-
-        $mock
-            ->expects($this->exactly(1))
-            ->method($method)
-            ->with(...$this->withConsecutive($arguments))
-            ->willReturn(
-                $response ?? new GuzzleResponse(200, [])
-            );
-
-        $this->app->instance(Client::class, $mock);
+        $this->mockClass(Client::class, [
+            [
+                'method' => $method,
+                'arguments' => $arguments,
+                'result' =>  $response ?? new GuzzleResponse(200, [])
+            ]
+        ]);
     }
 }
