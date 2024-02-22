ffb03a84423504724e55fbd53e849917b4641622
Vitaly Iskrin <viskrin@ronasit.com>
2024-02-21 22:37:18 -0600

    test: cover php9 mailable subject

--- src/Traits/MailsMockTrait.php
+++ src/Traits/MailsMockTrait.php
@@ -96,12 +96,22 @@ trait MailsMockTrait
         $expectedSubject = Arr::get($currentMail, 'subject');
 
         if (!empty($expectedSubject)) {
-            $this->assertEquals(
-                $expectedSubject,
-                $mail->subject,
-                "Failed assert that the expected subject \"{$expectedSubject}\" equals "
-                . "to the actual \"{$mail->subject}\"."
-            );
+            if (method_exists($mail, 'hasSubject')) {
+                $subject = method_exists($mail, 'envelope') ? $mail->envelope()->subject : $mail->subject;
+
+                $this->assertTrue(
+                    $mail->hasSubject($expectedSubject),
+                    "Failed assert that the expected subject \"{$expectedSubject}\" equals "
+                    . "to the actual \"{$subject}\"."
+                );
+            } else {
+                $this->assertEquals(
+                    $expectedSubject,
+                    $mail->subject,
+                    "Failed assert that the expected subject \"{$expectedSubject}\" equals "
+                    . "to the actual \"{$mail->subject}\"."
+                );
+            }
         }
     }
 
