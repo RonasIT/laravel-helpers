5ce3a7e413c6c90f9ed6689090c8b50e3e919b1a
Evgeny Ulin <eulin@ronasit.com>
2024-06-08 21:28:03 +0300

    #124: Wrote test for the helper that generates test state.

---
+++ tests/support/SampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace RonasIT\Support\Tests\Support;
+
+use RonasIT\Support\Tests\TestCase;
+
+class SampleTest extends TestCase
+{
+    public function createApplication()
+    {
+    }
+}
