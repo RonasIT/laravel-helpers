c98fdf28354d1eb6f6ce0c5e785e962b82bd50f3
DenTray <dpankratov@ronasit.com>
2024-12-12 06:40:10 +0600

    Update tests/support/Traits/TableTestStateMockTrait.php

--- tests/support/Traits/TableTestStateMockTrait.php
+++ tests/support/Traits/TableTestStateMockTrait.php
@@ -15,7 +15,6 @@ trait TableTestStateMockTrait
         $builderMock = $this->mockClass(Builder::class, ['orderBy', 'get'], true);
 
         DB::shouldReceive('getDefaultConnection')->once()->andReturn(null);
-
         DB::shouldReceive('connection')->once()->andReturnSelf();
         DB::shouldReceive('table')->with('test_models')->once()->andReturn($builderMock);
 
