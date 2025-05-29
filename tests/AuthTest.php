1b8ef129a1fb1f498ac5c23bdb235698d0aae943
Ruslan Guskov <feniksuh@gmail.com>
2025-05-29 17:07:49 +0300

    refactor: remove TestCaseMock class

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -2,68 +2,32 @@
 
 namespace RonasIT\Support\Tests;
 
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Auth;
 use RonasIT\Support\Tests\Support\Mock\Models\MockAuthUser;
-use RonasIT\Support\Traits\FixturesTrait;
-use RonasIT\Support\Tests\Support\Mock\TestCaseMock;
+use RonasIT\Support\Testing\TestCase as PackageTestCase;
 
 class AuthTest extends TestCase
 {
-    use FixturesTrait;
-
-    protected TestCaseMock $testCaseMock;
-
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        $this->testCaseMock = (new TestCaseMock('name'))
-            ->setUpMock($this->app);
-    }
-
-    public function testActingViaSession()
-    {
-        $userId = 1;
-
-        $this->testCaseMock->actingViaSession($userId);
-
-        $session = $this->app['session']->getDrivers()['array'];
-        $loginSession = $this->getLoginSession($session);
-
-        $this->assertNotEmpty($loginSession);
-        $this->assertEquals('laravel_session', $session->getName());
-        $this->assertEquals(array_values($loginSession), [$userId]);
-    }
-
-    public function testActingViaSessionDifferentGuard()
-    {
-        $userId = 1;
-
-        $this->testCaseMock->actingViaSession($userId, 'some_guard');
-
-        $session = $this->app['session']->getDrivers()['array'];
-        $loginSession = $this->getLoginSession($session, 'some_guard');
-
-        $this->assertNotEmpty($loginSession);
-        $this->assertEquals('laravel_session', $session->getName());
-        $this->assertEquals(array_values($loginSession), [$userId]);
-    }
-
-    public function testActingWithEmptySession()
-    {
-        $session = Arr::get($this->app['session']->getDrivers(), 'array', collect());
-        $loginSession = $this->getLoginSession($session);
-
-        $this->assertEmpty($loginSession);
-    }
-
     public function testActingAs()
     {
         $mockedUser = new MockAuthUser();
 
-        $this->testCaseMock->actingAs($mockedUser);
+        $mock = $this
+            ->getMockBuilder(PackageTestCase::class)
+            ->onlyMethods(['be'])
+            ->setConstructorArgs(['name'])
+            ->getMock();
 
-        $this->assertNotSame($mockedUser, Auth::user());
+        $mock->expects($this->once())
+            ->method('be')
+            ->with($this->callback(function ($cloneMockedUser) use ($mockedUser) {
+                $this->assertNotSame($mockedUser, $cloneMockedUser);
+
+                $this->assertEquals($mockedUser, $cloneMockedUser);
+
+                return true;
+            }))
+            ->willReturn($mock);
+
+        $mock->actingAs($mockedUser);
     }
 }
