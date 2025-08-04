8ebe9b9dc7fd870f51d31de92f906222a125160f
pirs1337 <malygin_555@mail.ru>
2025-08-04 11:49:48 +0600

    feat: Implement middleware to restrict IP access refs: https://github.com/RonasIT/laravel-helpers/issues/214

---
+++ src/Http/Middleware/CheckIpMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace RonasIT\Support\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class CheckIpMiddleware
+{
+    public function handle(Request $request, Closure $next, string ...$allowedIps): Response
+    {
+        $ips = [$request->header('x-real-ip'), $request->header('x-forwarded-for')];
+
+        if (empty(array_intersect(config('defaults.protected_endpoint_available_ips'), $ips)) && App::environment('production')) {
+            throw new AccessDeniedHttpException();
+        }
+
+        return $next($request);
+    }
+}
