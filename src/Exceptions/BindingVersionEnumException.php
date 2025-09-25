193e8bb6e0a0e83c373b872386106fc1123edc63
Anton Zabolotnikov <azabolotnikov@ronasit.com>
2025-09-25 18:10:13 +0500

    feat: update BindingVersionEnumException message

--- src/Exceptions/BindingVersionEnumException.php
+++ src/Exceptions/BindingVersionEnumException.php
@@ -9,9 +9,10 @@ class BindingVersionEnumException extends BindingResolutionException
     public function __construct()
     {
         parent::__construct(
-            'The VersionEnumContract is not bound in the container.'
-            . ' Please ensure it is registered using'
-            . ' $this->app->bind(VersionEnumContract::class, fn () => VersionEnum::class);'
+            "The VersionEnumContract is not bound in the container.\n"
+                . 'Please ensure it is registered correctly '
+                . "https://github.com/RonasIT/laravel-helpers/blob/master/documentation/versioning.md#step-2\n"
+                . 'More info here https://github.com/RonasIT/laravel-helpers/blob/master/documentation/versioning.md'
         );
     }
 }
