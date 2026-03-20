2c0f541102d298b698341c47d8c71aff1923c282
vgrams <vgrams@ronasit.com>
2026-03-20 17:19:27 +0600

    test: update JSONCustomCast with more complex structure to check cast->get() method calling when asserting model state changes;
    refs: https://github.com/RonasIT/laravel-helpers/issues/263

--- tests/support/Mock/Casts/JSONCustomCast.php
+++ tests/support/Mock/Casts/JSONCustomCast.php
@@ -6,13 +6,31 @@ use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 
 class JSONCustomCast implements CastsAttributes
 {
-    public function get($model, $key, $value, $attributes)
+    public function get($model, $key, $value, $attributes): array
     {
-        return json_decode($value);
+        $data = json_decode($value, true) ?? [];
+
+        return [
+            'appearance' => [
+                'theme' => $data['theme'] ?? null,
+            ],
+            'locale' => [
+                'language' => $data['language'] ?? null,
+            ],
+            'notifications' => [
+                'email' => $data['notifications_email'] ?? false,
+                'sms' => $data['notifications_sms'] ?? false,
+            ],
+        ];
     }
 
-    public function set($model, $key, $value, $attributes)
+    public function set($model, $key, $value, $attributes): string
     {
-        return json_encode($value);
+        return json_encode([
+            'theme' => $value['appearance']['theme'] ?? null,
+            'language' => $value['locale']['language'] ?? null,
+            'notifications_email' => $value['notifications']['email'] ?? false,
+            'notifications_sms' => $value['notifications']['sms'] ?? false,
+        ]);
     }
 }
