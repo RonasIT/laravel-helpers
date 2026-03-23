be06f64591bc8e1f612eab829a8695155b3f3b70
DenTray <dpankratov@ronasit.com>
2026-03-23 11:57:03 +0600

    Apply suggestions from code review
    Co-authored-by: DenTray <pankratov.denis@mail.ru>

--- src/Http/BaseRequest.php
+++ src/Http/BaseRequest.php
@@ -34,9 +34,9 @@ class BaseRequest extends FormRequest
 
         $validatedFields = $this->filterOnlyValidated(parent::all(), array_undot(array_flip($rules)));
 
-        return (empty($keys)
+        return (empty($keys))
             ? $validatedFields
-            : Arr::only($validatedFields, $keys));
+            : Arr::only($validatedFields, $keys);
     }
 
     protected function getOrderableFields(string $modelName, array $additionalFields = []): string
