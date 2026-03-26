c838ca3899a97863634744113bf642fdfada2eb6
DenTray <dpankratov@ronasit.com>
2026-03-26 08:01:58 +0600

    Apply suggestion from @DenTray

--- tests/FixturesTraitTest.php
+++ tests/FixturesTraitTest.php
@@ -29,7 +29,6 @@ class FixturesTraitTest extends TestCase
         parent::setUp();
 
         self::$tables = null;
-        self::$sequences = [];
     }
 
     public static function getFixtureData(): array
