12160f19e3824593c73a55763ae313229b432c68
Artyom Osepyan <art@osepyan.ru>
2026-03-30 20:47:45 +0300

    refactor: mark exceptions as deprecated (moved to EntityGenerator package)

--- src/Exceptions/EntityCreateException.php
+++ src/Exceptions/EntityCreateException.php
@@ -4,6 +4,9 @@ namespace RonasIT\Support\Exceptions;
 
 use Exception;
 
+/**
+ * @deprecated Class was moved to the EntityGenerator package as it is used there.
+ */
 class EntityCreateException extends Exception
 {
 }
