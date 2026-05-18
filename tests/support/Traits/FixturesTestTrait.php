3d7193b3b772cabe8ce75612fe4357bfd72b0f42
vgrams <vgrams@ronasit.com>
2026-05-18 12:40:55 +0600

    refactor: use Mockery to mock app()->version() call instead of anonymous class

--- tests/support/Traits/FixturesTestTrait.php
+++ tests/support/Traits/FixturesTestTrait.php
@@ -3,23 +3,16 @@
 namespace RonasIT\Support\Tests\Support\Traits;
 
 use Illuminate\Database\DatabaseManager;
+use Illuminate\Foundation\Application;
+use Mockery;
 use PHPUnit\Framework\MockObject\MockObject;
 
 trait FixturesTestTrait
 {
     public function mockLaravelVersion(string $version): void
     {
-        $app = new readonly class($version)
-        {
-            public function __construct(private string $version)
-            {
-            }
-
-            public function version(): string
-            {
-                return $this->version;
-            }
-        };
+        $app = Mockery::mock(Application::class);
+        $app->shouldReceive('version')->andReturn($version);
 
         $this->mockNativeFunction('RonasIT\Support\Traits', $this->functionCall('app', [], $app));
     }
