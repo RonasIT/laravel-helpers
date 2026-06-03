3a8b5fc016bcf5ec054d7193f45c72939675e324
Anton Zabolotnikov <110885041+AZabolotnikov@users.noreply.github.com>
2026-06-03 13:18:27 +0500

    Update tests/support/Mock/Casts/BinaryCast.php
    Co-authored-by: DenTray <dpankratov@ronasit.com>

--- tests/support/Mock/Casts/BinaryCast.php
+++ tests/support/Mock/Casts/BinaryCast.php
@@ -8,7 +8,7 @@ class BinaryCast implements CastsAttributes
 {
     public function get($model, $key, $value, $attributes)
     {
-        return is_null($value) ? $value : bin2hex($value);
+        return (is_null($value)) ? $value : bin2hex($value);
     }
 
     public function set($model, $key, $value, $attributes)
