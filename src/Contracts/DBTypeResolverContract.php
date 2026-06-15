8d05e579d3da5391eea927584bce9df8b5731985
DenTray <dpankratov@ronasit.com>
2026-06-15 11:08:30 +0600

    Apply suggestion from @DenTray

--- src/Contracts/DBTypeResolverContract.php
+++ src/Contracts/DBTypeResolverContract.php
@@ -7,7 +7,7 @@ use RonasIT\Support\Enums\DBTypeCategoryEnum;
 interface DBTypeResolverContract
 {
     /**
-     * @return array{0: int|float|string, 1: int|float|string}
+     * @return array{0: int|float, 1: int|float}
      */
     public function getRange(string $type): array;
 
